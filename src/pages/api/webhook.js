df22b429d7f0363395d0b0c664100b1381afc3c7
Mahmoud Qaddura <65426089+ApoQais@users.noreply.github.com>
2023-05-30 06:53:09 +0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,8 +1,1 @@
-import { buffer } from "micro/types/src/lib";
-//import db admin and install in npm
-//give auth to admin
-export default async (req, res) => {
-  if (req.method === "POST") {
-  }
-
-};
+//todo
