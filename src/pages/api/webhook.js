24e39b2cd170f882883ae82e7c29891c9c0ceeb4
Mahmoud Qaddura <65426089+MahmoudQaddura@users.noreply.github.com>
2023-06-20 14:41:52 +0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -41,8 +41,8 @@ export default async function handler(req, res) {
         quantity: formatted_quantity,
       };
 
-      registerOrder(report);
       res.status(200);
+      registerOrder(report);
     }
   }
 }
